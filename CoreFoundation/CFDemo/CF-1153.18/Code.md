4f19c5e12629c93a03cb6862c38ecef738245d37
ripper <453942056@qq.com>
2021-10-23 23:03:49 +0800

    添加 cf

---
+++ CoreFoundation/CFDemo/CF-1153.18/Code.md
@@ -0,0 +1,2 @@
+代码下载地址：https://opensource.apple.com/tarballs/CF
+版本号：CF-1153.18
---
+++ CoreFoundation/CFDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CFDemo
+//
+//  Created by ripper on 2021/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
